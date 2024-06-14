275a6ba3687b7ea3e2f729ec85fb42b24fae1033
Grzegorz Krajniak <gkrajniak@gmail.com>
2024-06-14 12:43:44 +0200

    Portal server lib ConfigController

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,14 @@ export * from './portal.module';
 export { EnvService } from './env/env.service';
 export { EnvVariablesService } from './env/env-variables.service';
 export { HealthChecker } from './health/health-checker';
+export {
+  EntityNotFoundException,
+  EntityContextProvider,
+} from './config/context/entity-context-provider';
+export { FrameContextProvider } from './config/context/frame-context-provider';
+export { TenantService } from './auth/tenant.service';
+export { HeaderParserService } from './services/header-parser.service';
+export {
+  ServiceProviderResponse,
+  ServiceProviderService,
+} from './config/context/service-provider';
