bd50e3898e8dd64b4b634678b5a9c4acc57aac61
Rafał Rogoda <rafal.rogoda@gmail.com>
2024-06-28 08:59:59 +0200

    Add default logout redirect url

--- src/env/env.service.ts
+++ src/env/env.service.ts
@@ -10,7 +10,7 @@ export class EnvService {
   public getEnv(): EnvVariables {
     return {
       healthCheckInterval: parseInt(process.env.HEALTH_CHECK_INTERVAL, 10),
-      logoutRedirectUrl: process.env.LOGOUT_REDIRECT_URL,
+      logoutRedirectUrl: process.env.LOGOUT_REDIRECT_URL || '/logout',
     };
   }
 }
