8b5d6fe5809b6b42c2ccd0f2e765d17889c4d460
gkrajniak <gkrajniak@gmail.com>
2024-06-28 16:04:54 +0200

    migrate AuthController

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,3 +3,4 @@ export { AuthController } from './auth.controller';
 export * from './auth.callback';
 export { AuthTokenService, AuthTokenResponse } from './auth-token.service';
 export { AuthCallback } from './auth.callback';
+export { TenantService } from './tenant.service';
