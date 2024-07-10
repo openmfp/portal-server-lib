da46033896d0bb16e28b13fb2aa63593b977dfd6
gkrajniak <gkrajniak@gmail.com>
2024-07-10 16:52:43 +0200

    Migrate ConfigController

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ export { FeatureTogglesProvider } from './config/context/feature-toggles-provide
 export { IntentResolveService } from './config/luigi/luigi-data/intent-resolve.service';
 export { LuigiDataService } from './config/luigi/luigi-data/luigi-data.service';
 export * from './config/model/luigi.node';
-export * from './config/model/luigi-app-config';
 export { TenantService } from './auth/tenant.service';
 export { HeaderParserService } from './services/header-parser.service';
 export {
