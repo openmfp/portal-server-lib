22fc87e86d31b3e0a25a9f6e46ef9d630ee28c64
tobias-oetzel <tobias.oetzel@gmail.com>
2024-07-16 08:15:04 +0200

    configure coverage thresholds (#13)
    * configure coverage thresholds
    
    ---------
    
    Co-authored-by: Tobias Oetzel <tobias.oetzel@sap.com>

--- jest.config.ts
+++ jest.config.ts
@@ -4,15 +4,24 @@ module.exports = {
   ...baseConfig,
   rootDir: 'src',
   testRegex: '.spec.ts$',
+  collectCoverage: true,
   reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputName: 'TEST-unit-tests-results.xml',
-        outputDirectory: 'test-run-reports',
-      },
-    ],
+    'default'
+  ],
+  coverageThreshold: {
+    global: {
+      // branches have an issue that controllers only get 50% coverage despite
+      // being covered 100% in the tests.
+      branches: 50,
+      functions: 80,
+      lines: 95,
+      // more than 10 uncovered statements
+      statements: -10,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/integration-tests/'
   ],
   coverageDirectory: '../test-run-reports/coverage/unit',
 };
