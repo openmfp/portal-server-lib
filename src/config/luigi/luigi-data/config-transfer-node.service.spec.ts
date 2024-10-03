79158cddc8195c648db9baaed02d62b5c9dd9dab
Grzegorz Krajniak <gkrajniak@gmail.com>
2024-10-03 12:43:43 +0200

    feat: content configuration data format processing

--- src/config/luigi/luigi-data/config-transfer-node.service.spec.ts
+++ src/config/luigi/luigi-data/config-transfer-node.service.spec.ts
@@ -1,17 +1,21 @@
 import { RawServiceProvider } from '../../context/service-provider';
-import { LuigiConfigFragment } from '../../model/content-configuration';
+import {
+  LuigiConfigData,
+  LuigiConfigFragment,
+} from '../../model/content-configuration';
+import { CrossNavigationInbounds, LuigiNode } from '../../model/luigi.node';
 import { ConfigTransferNodeService } from './config-transfer-node.service';
 import { IntentResolveService } from './intent-resolve.service';
 
 describe('ConfigTransferNodeService', () => {
   let service: ConfigTransferNodeService;
-  let intentResolveService: IntentResolveService;
+  let intentResolveServiceMock: IntentResolveService;
 
   beforeEach(() => {
-    intentResolveService = {
+    intentResolveServiceMock = {
       resolve: jest.fn(),
     } as any as IntentResolveService;
-    service = new ConfigTransferNodeService(intentResolveService);
+    service = new ConfigTransferNodeService(intentResolveServiceMock);
   });
 
   describe('isAbsoluteUrl', () => {
@@ -69,6 +73,88 @@ describe('ConfigTransferNodeService', () => {
         luigiConfigFragment.data.nodes[0]._userSettingsConfig.groups.general
           .viewUrl
       ).toBe('https://app.example.com/settings');
+    });
+
+    it('should not modify anything if nodes array is empty', () => {
+      const nodes: LuigiNode[] = [];
+      const luigiConfigData: LuigiConfigData = {} as LuigiConfigData;
+      const urlTemplateUrl = 'https://example.com/';
+
+      service.transferConfig(nodes, luigiConfigData, urlTemplateUrl);
+
+      expect(nodes).toEqual([]);
+      expect(intentResolveServiceMock.resolve).not.toHaveBeenCalled();
+    });
+
+    it('should set _preloadUrl if viewGroup.preloadSuffix exists', () => {
+      const nodes: LuigiNode[] = [{}];
+      const luigiConfigData: LuigiConfigData = {
+        viewGroup: { preloadSuffix: 'preload' },
+      } as LuigiConfigData;
+      const urlTemplateUrl = 'https://example.com/';
+
+      service.transferConfig(nodes, luigiConfigData, urlTemplateUrl);
+
+      expect(nodes[0]._preloadUrl).toBe('https://example.com/preload');
+    });
+
+    it('should set _requiredIFramePermissionsForViewGroup if it exists', () => {
+      const nodes: LuigiNode[] = [{}];
+      const luigiConfigData: LuigiConfigData = {
+        viewGroup: { requiredIFramePermissions: ['camera', 'microphone'] },
+      } as any as LuigiConfigData;
+      const urlTemplateUrl = 'https://example.com/';
+
+      service.transferConfig(nodes, luigiConfigData, urlTemplateUrl);
+
+      expect(nodes[0]._requiredIFramePermissionsForViewGroup).toEqual([
+        'camera',
+        'microphone',
+      ]);
+    });
+
+    it('should set _userSettingsConfig and update relative viewUrls', () => {
+      const nodes: LuigiNode[] = [{}];
+      const luigiConfigData: LuigiConfigData = {
+        userSettings: {
+          groups: {
+            group1: { viewUrl: 'settings1' },
+            group2: { viewUrl: 'https://external.com/settings2' },
+          },
+        },
+      } as any as LuigiConfigData;
+      const urlTemplateUrl = 'https://example.com/';
+
+      service.transferConfig(nodes, luigiConfigData, urlTemplateUrl);
+
+      expect(nodes[0]._userSettingsConfig).toEqual({
+        groups: {
+          group1: { viewUrl: 'https://example.com/settings1' },
+          group2: { viewUrl: 'https://external.com/settings2' },
+        },
+      });
+    });
+
+    it('should call intentResolveService.resolve with correct parameters', () => {
+      const nodes: LuigiNode[] = [{}];
+      const luigiConfigData: LuigiConfigData = {
+        targetAppConfig: {
+          appCrossNavigation: {
+            crossNavigation: {
+              inbounds: {} as CrossNavigationInbounds,
+            },
+          },
+        },
+      } as any as LuigiConfigData;
+      const urlTemplateUrl = 'https://example.com/';
+
+      service.transferConfig(nodes, luigiConfigData, urlTemplateUrl);
+
+      expect(intentResolveServiceMock.resolve).toHaveBeenCalledWith(
+        nodes,
+        luigiConfigData.targetAppConfig?.appCrossNavigation?.crossNavigation
+          ?.inbounds
+      );
     });
   });
 });
