30c19b633243cb68b9c2214e302ba6a4ddaa4103
Rafał Rogoda <rafal.rogoda@gmail.com>
2024-10-07 15:58:54 +0200

    feature/add-local-configuration

--- src/local-nodes/local-nodes.controller.ts
+++ src/local-nodes/local-nodes.controller.ts
@@ -1,10 +1,18 @@
+import { Request, Response } from 'express';
 import { Controller, Get, Logger, Req, Res } from '@nestjs/common';
 import { of } from 'rxjs';
-import { LuigiNode } from 'src/config';
+import {
+  ContentConfiguration,
+  ContentConfigurationLuigiDataService,
+  LuigiNode,
+} from 'src/config';
 
 @Controller('/rest/localnodes')
 export class LocalNodesController {
-  constructor(private logger: Logger) {}
+  constructor(
+    private logger: Logger,
+    private luigiDataService: ContentConfigurationLuigiDataService
+  ) {}
 
   @Get()
   async getLocalNodes(
@@ -12,8 +20,20 @@ export class LocalNodesController {
     @Res({ passthrough: true }) response: Response
   ): Promise<LuigiNode[]> {
     try {
-      //TODO get luigi nodes
-      const nodes: LuigiNode[] = [];
+      const language: string = request.query.language;
+      const data: { value: ContentConfiguration }[] = JSON.parse(request.query.contentConfigurations);
+      const contentConfigurations = data.map((cc) => cc.value);
+
+      const nodes: LuigiNode[] = await this.luigiDataService.getLuigiData(
+        {
+          name: 'localContentConfiguration',
+          displayName: 'localContentConfiguration',
+          contentConfiguration: contentConfigurations,
+          config: {},
+          creationTimestamp: Date.now().toString(),
+        },
+        language
+      );
 
       return of(nodes).toPromise();
     } catch (e) {
