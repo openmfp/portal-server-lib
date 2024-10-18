3d0938cb38af192225dbac15f2ba08f9f5d1a9c4
Grzegorz Krajniak <gkrajniak@gmail.com>
2024-10-18 13:55:35 +0200

    feat: use refresh token for automatic session refresh

--- src/env/env-variables.service.spec.ts
+++ src/env/env-variables.service.spec.ts
@@ -1,12 +1,11 @@
 import { EnvVariablesServiceImpl } from './env-variables.service';
-import { AuthDataService, AuthTokenData } from '../auth';
+import { AuthTokenData } from '../auth';
 import { Request, Response } from 'express';
 import { EnvService } from './env.service';
 
 describe('EnvVariablesServiceImpl', () => {
   let envVariablesService: EnvVariablesServiceImpl;
   let envServiceMock: EnvService;
-  let authDataServiceMock: jest.Mocked<AuthDataService>;
   let mockRequest: Partial<Request>;
   let mockResponse: Partial<Response>;
 
@@ -24,69 +23,31 @@ describe('EnvVariablesServiceImpl', () => {
   };
 
   beforeEach(() => {
-    authDataServiceMock = {
-      provideAuthData: jest.fn(),
-    } as any;
-
     envServiceMock = {
       getCurrentAuthEnv: jest.fn().mockReturnValue(currentAuthEnv),
       getEnv: jest.fn().mockReturnValue(env),
     } as any;
 
-    envVariablesService = new EnvVariablesServiceImpl(
-      authDataServiceMock,
-      envServiceMock
-    );
+    envVariablesService = new EnvVariablesServiceImpl(envServiceMock);
 
     mockRequest = {};
     mockResponse = {};
   });
 
   describe('getEnv', () => {
-    it('should call authDataService.provideAuthData', async () => {
-      const result = await envVariablesService.getEnv(
-        mockRequest as Request,
-        mockResponse as Response
-      );
-
-      expect(authDataServiceMock.provideAuthData).toHaveBeenCalledWith(
-        mockRequest,
-        mockResponse
-      );
-    });
-
     it('should return the result from authDataService.provideAuthData and current auth envs', async () => {
-      const mockAuthData = {
-        access_token: 'testAccessToken',
-        refresh_token: 'testRefreshToken',
-      } as AuthTokenData;
-      authDataServiceMock.provideAuthData.mockResolvedValue(mockAuthData);
-
       const result = await envVariablesService.getEnv(
         mockRequest as Request,
         mockResponse as Response
       );
 
       expect(result).toEqual({
-        authData: mockAuthData,
         ...currentAuthEnv,
         validWebcomponentUrls: 'validWebcomponentUrls',
         logoutRedirectUrl: 'logoutRedirectUrl',
         isLocal: true,
         developmentInstance: true,
       });
-    });
-
-    it('should handle errors from authDataService.provideAuthData', async () => {
-      const mockError = new Error('Auth data error');
-      authDataServiceMock.provideAuthData.mockRejectedValue(mockError);
-
-      await expect(
-        envVariablesService.getEnv(
-          mockRequest as Request,
-          mockResponse as Response
-        )
-      ).rejects.toThrow('Auth data error');
     });
 
     it('should return a promise', () => {
