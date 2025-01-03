1c119e7ae49b889c66353f5ef85e3b66da103d77
Rafał Rogoda <rafal.rogoda@gmail.com>
2025-01-03 11:40:26 +0100

    update

--- src/config/luigi/luigi-data/content-configuration-validator.service.spec.ts
+++ src/config/luigi/luigi-data/content-configuration-validator.service.spec.ts
@@ -1,27 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContentConfigurationValidatorService } from './content-configuration-validator.service';
-import { LOCAL_NODES_VALIDATOR_INJECTION_TOKEN } from '../../../injection-tokens';
-import { mock, MockProxy } from 'jest-mock-extended';
-import { LocalNodesValidatorService } from '../../../local-nodes';
+import { LocalNodesValidatorService, LocalNodesValidatorServiceImpl, ValidationResult } from '../../../local-nodes';
 import { Logger } from '@nestjs/common';
+import { ContentConfiguration, LOCAL_NODES_VALIDATOR_INJECTION_TOKEN } from '../../../';
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
+import { mock, MockProxy } from 'jest-mock-extended';
 
 describe('ContentConfigurationValidatorService', () => {
   let service: ContentConfigurationValidatorService;
-  let localNodesValidatorMock: MockProxy<LocalNodesValidatorService>;
+  let localNodesValidatorService: MockProxy<LocalNodesValidatorServiceImpl>;
 
   beforeEach(async () => {
-    localNodesValidatorMock = mock<LocalNodesValidatorService>();
+    localNodesValidatorService = mock<LocalNodesValidatorServiceImpl>();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: LOCAL_NODES_VALIDATOR_INJECTION_TOKEN,
-          useValue: localNodesValidatorMock,
+          useValue: localNodesValidatorService,
         },
         Logger,
         ContentConfigurationValidatorService,
       ],
     }).compile();
-
     service = module.get<ContentConfigurationValidatorService>(
       ContentConfigurationValidatorService
     );
@@ -32,6 +33,40 @@ describe('ContentConfigurationValidatorService', () => {
   });
 
   describe('validateContentConfiguration', () => {
+    it('should execute validateContentConfiguration', async () => {
+      //Arrange
+      const contentConfigurations: ContentConfiguration[] = [
+        {
+          name: 'example 1',
+          creationTimestamp: '',
+          luigiConfigFragment: undefined
+        },
+        {
+          name: 'example 2',
+          creationTimestamp: '',
+          luigiConfigFragment: undefined
+        }
+      ];
 
+      const expectedResult: AxiosResponse<ValidationResult, any> = {
+        status: 200,
+        statusText: '',
+        data: {
+          parsedConfiguration: "{\"name\":\"example\",\"luigiConfigFragment\":{\"data\":{\"nodes\":[],\"texts\":[]}}}",
+        },
+        headers: {},
+        config: undefined,
+      };
+
+      jest.spyOn(localNodesValidatorService, 'validateContentConfiguration')
+      .mockReturnValue(of(expectedResult));
+
+      //Act
+      const result = await service.validateContentConfiguration(contentConfigurations);
+
+      //Assert
+      expect(result).toEqual([expectedResult, expectedResult]);
+
+    });
   });
 });
