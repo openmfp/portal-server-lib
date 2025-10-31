ffd26b5124a3d7f19cf02ee9c22df9c37d64fcac
Sobyt483 <117839500+Sobyt483@users.noreply.github.com>
2025-10-31 11:17:07 +0300

    fix: add missing import (#308)

--- src/config/context/request-context-provider.ts
+++ src/config/context/request-context-provider.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import type { Request } from 'express';
+import type { Request, Response } from 'express';
 
 export interface RequestContextProvider {
   getContextValues(
